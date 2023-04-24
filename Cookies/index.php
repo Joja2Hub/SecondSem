09619b8e460c5cb8ca0c28493c1675df6fe2fe8c
Ivan <112862682+Joja2Hub@users.noreply.github.com>
2023-04-24 10:15:11 +0300

    Update index.php

--- Cookies/index.php
+++ Cookies/index.php
@@ -169,25 +169,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         setcookie('biography_error', '', 100000);
         setcookie('signed_error', '', 100000);
     }
-    //*************************
+     //*************************
 
     $user = 'u52863';
     $pass = '7320376';
-    
-    try {
-        $database = new PDO('mysql:host=localhost;dbname=u52863', $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    } catch (PDOException $e) {
-        die($e->getMessage());
-    }
-    
-    
-    $statement = $database -> prepare("INSERT INTO Person (name, email, date, gender, limb, biography, signed) VALUES (:name, :email, :date, :gender, :limb, :biography, :signed)");
-            $statement -> execute(['name' => $_POST['name'], 'email' => $_POST['email'], 'date' => $_POST['date'], 'gender' => $_POST['gender'], 'limb' => $_POST['limb'], 'biography' => $_POST['biography'], 'signed' => $_POST['signed']]);
-            $id_connection = $database -> lastInsertId();
+
+try {
+    $database = new PDO('mysql:host=localhost;dbname=u52863', $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+        $statement = $database -> prepare("INSERT INTO Person (name, email, date, gender, limb, biography, signed) VALUES (:name, :email, :date, :gender, :limb, :biography, :signed)");
+        $statement -> execute(['name' => $_POST['name'], 'email' => $_POST['email'], 'date' => $_POST['date'], 'gender' => $_POST['gender'], 'limb' => $_POST['limb'], 'biography' => $_POST['biography'], 'signed' => $_POST['signed']]);
+        $id_connection = $database -> lastInsertId();
+        $statement = $database -> prepare("INSERT INTO Connection (id_person, id_abil) VALUES (:id_person, :id_abil)");
+        foreach($_POST['superpowers'] as $superpowers){
+            if($superpowers != false){
+                $statement -> execute(['id_person' => $id_connection, 'id_abil' => $superpowers]);
+            }
+        }
     if ($database->connect_error){
-        echo "Error Number: ".$database->connect_errno."<br>";
-        echo "Error: ".$database->connect_error;
-    }
+    echo "Error Number: ".$database->connect_errno."<br>";
+    echo "Error: ".$database->connect_error;
+    }   
+    
 
     //*************************
 
